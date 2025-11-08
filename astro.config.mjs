4303bb3fec81b15c37f0f8c839e0c66039db8c0f
Jonathan R. Reed <Contact@JonathanRReed.com>
2025-11-08 12:08:03 -0600

    i'm dumb

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://5whys.jonathanrreed.com',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
